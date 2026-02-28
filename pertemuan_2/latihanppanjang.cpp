346992d83f28b7955418fb8540ae84f536fe167c
Montela11111 <montelaaberil@gmail.com>
2026-02-28 09:38:13 +0700

    Add files via upload

---
+++ pertemuan_2/latihanppanjang.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float luas, p, l;
+
+    cout << "Masukan panjang: ";
+    cin >> p;
+
+    cout << "Masukan lebar: ";
+    cin >> l;
+
+    luas = p*l ;
+    cout << " luas : " << luas << endl;
+    
+
+
+
+
+}
