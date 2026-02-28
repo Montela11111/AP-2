6487e99671afc76bcbed079fa93c96a43c51f5ec
Montela11111 <montelaaberil@gmail.com>
2026-02-28 09:49:22 +0700

    Penggunaan Operator

---
+++ pertemuan_2/operator.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    system("cls");
+
+    // Assignment Operator, " = ", ngasih nilai ke dlm suatu variabel
+    a = 3;
+    b = 5;
+
+    // // Aritmatical operator, operator untuk operasi matematika 
+    // int tambah = a + b;
+    // int kurang = a - b;
+    // int kali   = a * b;
+    // // float bagi = a / b;
+    // float bagi = (float) a/ (float)b; //type casting mengubah tipe data secara paksa
+    // int modulo = a % b;
+
+    // cout << "Hasil Penjumalahn : " << tambah << endl;
+    // cout << "Hasil Pengurangan : " << kurang << endl;
+    // cout << "Hasil Perkalian : " << kali << endl;
+    // cout << "Hasil Pembagian : " << bagi << endl;
+    // cout << "Hasil sisa bagi  : " << modulo << endl;
+
+    // Relational operator, membandingkan 2 nilai dari 2 operand. Hasil berupa boolean yaitu 0/1 true dan false
+    // cout << (a == b) << endl;
+    // cout << ( a < b) << endl;
+    // cout << (a != b) << endl;
+
+    // Logical Operator (&&, ||, !), bandingkan 2 nilai variabel yang bertipe boolean
+    // cout << (true && true ) << endl;
+    // cout << (true && false) << endl;
+
+    // cout << (true || false) << endl;
+    // cout << (false || false) << endl;
+
+    // cout << !true  << endl;
+    // cout << !false << endl;
+
+    // Bitwise Operator > untuk operasi biner (&, |, ^, ~, >>, <<)
+    // cout << ( 5 & 7 ) << endl;
+    // cout << ( 5 | 7 ) << endl;
+    // cout << ( 5 ^ 7 ) << endl;
+    // cout << (~7) << endl;
+    // cout << ( 7 << 2 ) << endl;
+    // cout << ( 7 >> 2 ) << endl;
+
+
+    // Operator Kondisional (?), Ternary, Kondisi -> hasil 1 ? hasil 2
+
+    // int max ;
+    // max = (a > b) ? a : b;
+    // cout << "Nilai Terbesar : " << max << endl;
+
+    // Increment , Decrement 
+    // Pre Increment ++ ada di dpn variabel 
+    // cout<< a << endl; //3
+    // cout<< ++a << endl; // ++3 =4
+
+    // // Post Increment ++ ada di belakang variabel 
+    // cout << a << endl; //3
+    // cout << a++ << endl; // 3+1
+    // cout << a << endl; //4
+
+    // Pre Decrement, -- ada di dpn var
+    cout << b << endl;
+    cout << b-- << endl;
+    cout << b << endl;
+
+
+
+
+
+
+
+
+
+
+
+}
