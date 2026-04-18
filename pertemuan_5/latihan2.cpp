a46e06394ff7b8d2fb15ddc552432eead61b0603
Montela11111 <montelaaberil@gmail.com>
2026-04-18 11:08:11 +0700

    Array & Vector

---
+++ pertemuan_5/latihan2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main (){
+    vector<string> nama = {"Nadia", "Echa", "Carlos","Ilham","Alan"};
+    cout <<"isi vector awal :" << endl;
+
+    for (int i : nama ) {
+        cout << i << endl;
+
+    }
+
+    
+}
+
